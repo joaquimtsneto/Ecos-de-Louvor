8cf620f9c7fd964947d7279e10d83d3a462fe932
joaquimtsneto <projetovoznodeserto@gmail.com>
2025-05-23 13:56:12 +0100

    Versao 17

--- script.js
+++ script.js
@@ -65,12 +65,5 @@ function exibirHinos(lista) {
   container.innerHTML = "";
   lista.forEach(h => {
     const bloco = document.createElement("div");
-    bloco.style.margin = "20px auto";
-    bloco.style.maxWidth = "700px";
-    bloco.style.textAlign = "left";
-    bloco.style.borderBottom = "1px solid #ccc";
-    bloco.style.paddingBottom = "10px";
-    bloco.innerHTML = `<h3>${h.numero} - ${h.titulo}</h3><pre style="white-space:pre-wrap">${h.letra}</pre>`;
-    container.appendChild(bloco);
-  });
-}
+    bloco.className = "hino-bloco";
+    
